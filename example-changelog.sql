1b17bec8e84184532ca4dd6899a2a919df7c10c1
mauricioramirezh <mauricio_ramirezh@bancolombia.com.co>
2022-04-21 16:05:36 -0500

    test liquibase

---
+++ example-changelog.sql
@@ -0,0 +1,14 @@
+--liquibase formatted sql
+--changeset dev:1
+CREATE TABLE cats_demo_two(
+    breed VARCHAR(255),
+    name varchar(255),
+    edge varchar(255)
+);
+
+--changeset dev:2
+CREATE TABLE cats_demo_three(
+    breed VARCHAR(255),
+    name varchar(255),
+    edge varchar(255)
+);
