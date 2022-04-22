3d7d76c6f9b49a72529f7fa76a74893794d048b1
mauricioramirezh <mauricio_ramirezh@bancolombia.com.co>
2022-04-22 15:46:31 -0500

    DEV - 50 prueba incidencia

--- example-changelog.sql
+++ example-changelog.sql
@@ -12,9 +12,25 @@ CREATE TABLE cats_demo_three(
     name varchar(255),
     edge varchar(255)
 );
---changeset dev:2
+-- changeset dev:3
+create table example1 ( id int primary key, name varchar(255) );
+
+--changeset dev:4
 CREATE TABLE cats_demo_four(
     breed VARCHAR(255),
     name varchar(255),
     edge varchar(255)
 );
+
+--changeset dev:6
+DROP TABLE cats_demo_four;
+
+--changeset dev:7
+CREATE TABLE cats_demo_four(
+    breed VARCHAR(255),
+    name varchar(255),
+    edge varchar(255)
+);
+
+--changeset dev:8
+Drop TABLE cats_demo_four;
